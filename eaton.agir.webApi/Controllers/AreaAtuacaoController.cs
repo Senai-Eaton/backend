b84e72dc98c92f2d3f5d83ca103cfdc3f3e9fede
LucasLima132 <lucas-limagomes@hotmail.com>
2018-03-13 20:40:07 -0300

    Crud modelo completo

--- eaton.agir.webApi/Controllers/AreaAtuacaoController.cs
+++ eaton.agir.webApi/Controllers/AreaAtuacaoController.cs
@@ -21,6 +21,7 @@ namespace eaton.agir.webApi.Controllers
             }
             
         }
+
         [HttpGet("{id}")]
         public IActionResult GetAction(int id){
             var area =_areaAtuacaoRepository.BuscarPorId(id);
@@ -28,6 +29,51 @@ namespace eaton.agir.webApi.Controllers
             return Ok(area);
             else 
             return NotFound();
+        }
+        [HttpPost]
+        public IActionResult Cadastrar([FromBody]AreaAtuacaoDomain area){
+            try{
+                _areaAtuacaoRepository.Inserir(area);
+                return Ok(area);
+            }catch(System.Exception ex){
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPut]
+        public IActionResult Atualizar(int id,[FromBody]AreaAtuacaoDomain area){
+            if (area==null ||area.Id !=id){
+                return BadRequest();
+            }
+            var area1 =_areaAtuacaoRepository.BuscarPorId(id);
+            if(area1== null){
+                return NotFound();
+
+            }
+            area1.Id= area.Id;
+            area1.Nome=area.Nome;
+            var rs= _areaAtuacaoRepository.Atualizar(area1);
+            if (rs>0) return Ok();
+            else return BadRequest();
+
+
+
+
+
+
+        }
+        [HttpDelete("{id}")]
+        public IActionResult delete(int id){
+            var areas=_areaAtuacaoRepository.BuscarPorId(id);
+            if(areas==null){
+                return NotFound ();
+            }
+
+            var rs = _areaAtuacaoRepository.Deletar(areas);
+            if (rs>0) return Ok();
+            else 
+           return BadRequest();
+            
+            
         }
         
     }
