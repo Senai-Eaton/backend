85f3cf418dec55d6a8866d32a49523b02a869ee7
LucasLima132 <lucas-limagomes@hotmail.com>
2018-03-13 18:45:39 -0300

    Contracts

---
+++ eaton.agir.domain/Contracts/IBaseRepository.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+namespace eaton.agir.domain.Contracts
+{
+    public interface IBaseRepository<T> where T : class
+    {
+         IEnumerable<T> Listar(string[] includes = null);
+         
+         int Atualizar(T dados);
+
+         int Inserir(T dados);
+
+         int Deletar(T dados);
+
+         T BuscarPorId(int id, string[] includes = null);
+    }
+    
+         
+    
+}
